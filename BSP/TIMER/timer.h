91cf1e060690bdad05c128334c2d4d966760c1ea
Acc <gucj@njust.edu.cn>
2025-10-23 10:49:55 +0800

    将PPS相关程序函数整合到pps.c内

--- BSP/TIMER/timer.h
+++ BSP/TIMER/timer.h
@@ -6,13 +6,11 @@
 #include "gpio.h"
 #include "uart_1.h"
 #include <string.h>
+#include "pps.h"
 
-extern uint8_t uart1_buf[256];
 
 void TIM_SetCCR(TIM_TypeDef *TIMx, uint32_t CHx, uint16_t CCR);
 void TIM3_IRQProcess(void);
 
-int checkNum(const char *gprmcContext);
-void print_PPS_GPS();
 
 #endif 
--- BSP/UART/uart_1.h
+++ BSP/UART/uart_1.h
@@ -5,6 +5,8 @@
 #include "usart.h"
 #include "gpio.h"
 
+extern uint8_t uart1_buf[256];
+
 //串口DMA发送方式（非阻塞），增加延时以保证数据的完整性	
 #define UART1_printf_DMA(...) HAL_UART_Transmit_DMA(&huart1,\
 												   (uint8_t *)uart1_buf,\
